25e71cfcf491bff9661de7156aa9c423edc35ccd
Denis1988 <denis1988@gmail.com>
2026-03-24 17:10:16 +0200

    posiible fixes of tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -7,11 +7,14 @@ CLI_SCRIPT = "python3 sawectl/sawectl.py"
 
 def test_init_module():
     """Test generating a new module."""
-    module_dir = "modules/test_slack_module"
+    # Note: Because we will run this inside the sawectl folder, 
+    # the module_dir should just be "modules/test_slack_module"
+    module_dir = "sawectl/modules/test_slack_module"
     if os.path.exists(module_dir):
         shutil.rmtree(module_dir)
         
-    result = subprocess.run(f"{CLI_SCRIPT} init module test_slack_module", shell=True, capture_output=True, text=True)
+    # Add cwd="sawectl" and run just "python3 sawectl.py"
+    result = subprocess.run("python3 sawectl.py init module test_slack_module", shell=True, capture_output=True, text=True, cwd="sawectl")
     assert result.returncode == 0, f"Init module failed: {result.stderr}"
     assert os.path.exists(f"{module_dir}/module.yaml")
     assert os.path.exists(f"{module_dir}/test_slack_module.py")
